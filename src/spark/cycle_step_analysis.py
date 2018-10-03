1ff441a3e6fc029822ee7f918b1e5350a37ac57b
Arthur Dysart <github@arthurdys.art>
2018-10-02 20:39:04 -0400

    updated spark streaming script [cycle_step_analysis.py]

--- src/spark/cycle_step_analysis.py
+++ src/spark/cycle_step_analysis.py
@@ -131,8 +131,8 @@ if __name__ == "__main__":
 
     # For each micro-RDD, strips whitespace and split by comma
     parsed_rdd = kafka_stream.map(lambda ln: ln.strip().split(","))
-    parsed_rdd.pprint()
-    print("PRINTED STATEMENT")
+    parsed_rdd.pprint(100)
+
     # For each micro-RDD, transforms instantaneous measurements to overall values in RDD
     summed_rdd = summarize_step_data(parsed_rdd)
     summed_rdd.persist()
